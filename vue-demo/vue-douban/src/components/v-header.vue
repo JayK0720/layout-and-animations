9d26ee4fa15ad904f78b64a80489d7eaef22c6cf
JayK0720 <1129594783@qq.com>
2019-12-22 22:29:10 +0800

    vue

---
+++ vue-demo/vue-douban/src/components/v-header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'v-header',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped lang='scss'>
+    .header{
+        heigit:30px;
+        line-height:30px;
+    }
+</style>
