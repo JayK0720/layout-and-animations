1208c94ae21f5c173be1d4f479d9fe1ce68e3b4a
JayK0720 <1129594783@qq.com>
2020-11-20 22:26:06 +0800

    修改拖拽交换逻辑; git commit -m 修改拖拽交换逻辑 git commit -m 修改拖拽交换逻辑

--- demo-drag-exchange/js/index.js
+++ demo-drag-exchange/js/index.js
@@ -15,7 +15,7 @@ oList.addEventListener('mousedown',drag,false);
 document.addEventListener('mousemove',drag,false);
 document.addEventListener('mouseup',drag,false);
 let flag = false;	// 判断是否点击了
-let startX,startY,x1,y1,element,gobal;
+let startX,startY,x1,y1,element,gobal,isExit;
 function drag(event){
 	event = event || window.event;
 	event.preventDefault();
@@ -33,6 +33,7 @@ function drag(event){
 			let x2 = event.clientX, y2 = event.clientY,delX,delY;
 			element.style.left = startX + x2 - x1 + 'px';
 			element.style.top = startY + y2 - y1 + 'px';
+			element.style.transition = '';
 			delX = x2 - oContainer.offsetLeft;
 			delY = y2 - oContainer.offsetTop;
 			for(let i = 0, length = aLi.length; i < length; i++){
@@ -41,7 +42,6 @@ function drag(event){
 					(delX > aLi[i].offsetLeft) && (delX < aLi[i].offsetLeft+220) 
 					&& (delY > aLi[i].offsetTop) && (delY < aLi[i].offsetTop+135) 
 				){
-					console.log("碰撞成功");
 					aLi[i].style.transform = 'scale(1.1)';
 					gobal = aLi[i];
 				}
@@ -50,16 +50,38 @@ function drag(event){
 		case "mouseup":
 			if(flag){
 				flag = false;
-				if(gobal) {
+				// 判断如果进入某个元素区域内后 又移除来 则不进行交换
+				let delX = event.clientX - oContainer.offsetLeft, delY = event.clientY - oContainer.offsetTop; 
+				function isExitTarget(){
+					for(let i = 0, length = aLi.length; i < length; i++){
+						if( element != aLi[i] &&
+							(delX > aLi[i].offsetLeft) && (delX < aLi[i].offsetLeft+220) 
+							&& (delY > aLi[i].offsetTop) && (delY < aLi[i].offsetTop+135) 
+						){
+							isExit = false;
+							return isExit;
+						}
+					}
+					return true;
+				}
+				let a = isExitTarget();
+				if(gobal && !a) {
 					element.style.left = gobal.offsetLeft + 'px';
 					element.style.top = gobal.offsetTop + 'px';
+					element.style.transition = 'all .3s';
 					gobal.style.left = startX + 'px';
 					gobal.style.top = startY + 'px';
+					gobal.style.transition = 'all .3s';
 					gobal.style.transform = 'scale(1)';
 					gobal = null;
 				}else{
 					element.style.left = startX + 'px';
 					element.style.top = startY + 'px';
+					element.style.transition = 'all .3s';
+					if(gobal){
+						gobal.style.transform = 'scale(1)';
+						gobal = null;
+					}
 				}
 			}
 			element.style.zIndex = 1;
